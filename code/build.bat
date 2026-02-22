936e9853cc4db284d7b83c63f54f76f81c1ac791
folayfila <abdofado@gmail.com>
2026-02-22 23:14:15 +0300

    ANIMATED RGB PIXELS

--- code/build.bat
+++ code/build.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\build
 pushd ..\build
-cl -Zi ..\code\win32_folayfila.cpp user32.lib Gdi32.lib
+cl -FC -Zi ..\code\win32_folayfila.cpp user32.lib Gdi32.lib
 popd
--- code/win32_folayfila.cpp
+++ code/win32_folayfila.cpp
@@ -1,71 +1,93 @@
 // (C) Copyright 2026 by Abdallah Maaliki / folayfila.
 
 #include <windows.h>
+#include <stdint.h>
 
 #define internal static         // Don't allow this function to be called from another file than its.
 #define local_presist static    // static local variable
 #define global_variable static
+
+typedef uint8_t uint8;
+typedef uint16_t uint316;
+typedef uint32_t uint32;
+typedef uint64_t uint364;
+
+typedef int8_t int8;
+typedef int16_t int316;
+typedef int32_t int32;
+typedef int64_t int364;
 
 // TODO(abdallah): Global for now.
 global_variable bool Running;
 
 global_variable BITMAPINFO BitmapInfo;
 global_variable void* BitmapMemory;
-global_variable HBITMAP BitmapHandle;
-global_variable HDC BitmapDeviceContext;
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+global_variable int BytesPerPixel = 4;
+
+internal void RenderAwesomeGradient(int XOffset, int YOffset)
+{
+    int Width = BitmapWidth;
+    int Height = BitmapHeight;
+
+    int Pitch = Width * BytesPerPixel;
+    uint8* Row = (uint8*)BitmapMemory;
+    for (int Y = 0; Y < BitmapHeight; ++Y)
+    {
+        uint32* Pixel = (uint32*)Row;
+        for (int X = 0; X < BitmapWidth; ++X)
+        {
+
+            uint8 Blue = (uint8)(X + XOffset);
+            uint8 Green = (uint8)(Y + YOffset);
+            uint8 Red = 100;
+
+            *Pixel++ = (Blue | Green << 8 | Red << 16);
+        }
+        Row += Pitch;
+    }
+}
 
 // We create a buffer for our content that we want to display, then we pass it to windows to paint it when we want.
 internal void Win32ResizeDIBSection(int Width, int Height)    // DIB: Devise Independent Bitmap
 {
-    // TODO(abd): Free the previous DIB
-    if (BitmapInfo.bmiHeader.biSize)
+    if (BitmapMemory)
     {
-        DeleteObject(BitmapHandle);
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
     }
 
-    if(!BitmapDeviceContext)
-    {
-        BitmapDeviceContext = CreateCompatibleDC(0);
-    }
+    BitmapWidth = Width;
+    BitmapHeight = Height;
 
     BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-    BitmapInfo.bmiHeader.biWidth = Width;
-    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biHeight = BitmapHeight;
     BitmapInfo.bmiHeader.biPlanes = 1;
     BitmapInfo.bmiHeader.biBitCount = 32;
-
     BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-    BitmapDeviceContext = CreateCompatibleDC(0);
-
-    BitmapHandle = CreateDIBSection(
-        BitmapDeviceContext, &BitmapInfo,
-        DIB_RGB_COLORS,
-        &BitmapMemory,
-        0, 0);
+    // Allocate memory ourselves. As long as it's the right size, windows will handle it.
+    // 4 bytes with an area of w*h
+    int BitmapMemorySize = (BitmapWidth*BitmapHeight) * BytesPerPixel ;
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 }
 
-internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+internal void Win32UpdateWindow(HDC DeviceContext, RECT* ClientRect, int X, int Y, int Width, int Height)
 {
+    int WindowWidth = ClientRect->right - ClientRect->left;
+    int WindowHeight = ClientRect->bottom - ClientRect->top;
     StretchDIBits(
         DeviceContext,
+        /*
         X, Y, Width, Height, // destination
         X, Y, Width, Height, // source
+        */
+        0, 0, BitmapWidth, BitmapHeight,
+        0, 0, WindowWidth, WindowHeight,
         BitmapMemory,
         &BitmapInfo,
         DIB_RGB_COLORS, SRCCOPY);
-}
-
-void Win32PaintWindow(HWND& Window)
-{
-    PAINTSTRUCT Paint;
-    HDC DeviceContext = BeginPaint(Window, &Paint);
-    int X = Paint.rcPaint.left;
-    int Y = Paint.rcPaint.top;
-    int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-    int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-    Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
-    EndPaint(Window, &Paint);
 }
 
 LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
@@ -81,35 +103,42 @@ LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WPara
             int Width = ClientRect.right - ClientRect.left;
             int Height = ClientRect.bottom - ClientRect.top;
             Win32ResizeDIBSection(Width, Height);
-            OutputDebugStringA("WM_SIZE\n");
         } break;
 
         case WM_ACTIVATEAPP:
         {
-            OutputDebugStringA("WM_ACTIVATEAPP\n");
         } break;
 
         case WM_PAINT:
         {
-            Win32PaintWindow(Window);
-            OutputDebugStringA("WM_PAINT\n");
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            
+            Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
+            
+            EndPaint(Window, &Paint);
         } break;
 
         case WM_DESTROY:
         {
             Running = false;
-            OutputDebugStringA("WM_DESTROY\n");
         } break;
 
         case WM_CLOSE:
         {
             Running = false;
-            OutputDebugStringA("WM_CLOSE\n");
         } break;
 
         default:
         {
-            OutputDebugStringA("default\n");
             Result = DefWindowProc(Window, Message, WParam, LParam);
         } break;
     }
@@ -135,7 +164,7 @@ int CALLBACK WinMain(
 
     if (RegisterClass(&WindowClass))
     {
-        HWND WindowHandle =
+        HWND Window =
             CreateWindowExA(
             0,
             WindowClass.lpszClassName,
@@ -152,21 +181,45 @@ int CALLBACK WinMain(
 
         // Main loop:
         Running = true;
-        if (WindowHandle)
+        if (Window)
         {
+            int ColorXOffset = 0;
+            int8 YSign = 1;
+            int ColorYOffset = 0;
             MSG Message;
             while (Running)
             {
-                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
-                if (MessageResult > 0)
+                while(PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
                 {
+                    if (Message.message == WM_QUIT)
+                    {
+                        Running = false;
+                    }
+
                     TranslateMessage(&Message);
                     DispatchMessage(&Message);
                 }
-                else
+
+                RenderAwesomeGradient(ColorXOffset, ColorYOffset);
+                HDC DeviceContext = GetDC(Window);
+                RECT ClientRect;
+                GetClientRect(Window, &ClientRect);
+                int WindowWidth = ClientRect.right - ClientRect.left;
+                int WindowHeight = ClientRect.bottom - ClientRect.top;
+                Win32UpdateWindow(DeviceContext, &ClientRect, 0, 0, WindowWidth, WindowHeight);
+                ReleaseDC(Window, DeviceContext);
+
+                // Simple zig-zag in y, kinda like sin.
+                if (ColorYOffset == 127)
                 {
-                    break;
+                    YSign = -1;
                 }
+                else if (ColorYOffset == -127)
+                {
+                    YSign = 1;
+                }
+                ColorYOffset += YSign;
+                ColorXOffset += 1;
             }
         }
         else
